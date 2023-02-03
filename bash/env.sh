be2c4595616c17eb36287d1fa4d68697a9a66780
Mark Dittmer <markdittmer@google.com>
2023-02-02 20:34:01 -0500

    More trying things out in scratch.sh

--- bash/env.sh
+++ bash/env.sh
@@ -14,6 +14,9 @@ export SWC_DIR="${POLA_JS_DIR}/swc"
 export SWC_BINDINGS_DIR="${SWC_DIR}/bindings"
 export SWC_CLI_DIR="${SWC_BINDINGS_DIR}/swc_cli"
 export SWC="${SWC_BINDINGS_DIR}/target/release/swc"
+export SEMAPHORIC_DIR="${POLA_JS_DIR}/semaphoric"
+export SEMAPHORIC="${SEMAPHORIC_DIR}/semaphoric"
+export ARTIFACT_EXECUTOR="${POLA_JS_DIR}/artifact-executor/bash/bin/artifact-executor"
 
 # TODO: Apache Ant-related paths below should not be hard-coded.
 export TAJS_CLASSPATH="${TAJS_DIR}/extras/inspector/dist/inspector.jar:${TAJS_DIR}/extras/jalangilogger/build/libs/jalangilogger-1.1.jar:${TAJS_DIR}/extras/ts-spec-reader/build/libs/ts-spec-reader.jar:${TAJS_DIR}/lib/closure-compiler-v20170806.jar:${TAJS_DIR}/lib/gson-2.8.1.jar:${TAJS_DIR}/lib/jericho-html-3.3.jar:${TAJS_DIR}/lib/jetty-all-9.4.8.v20171121-uber.jar:${TAJS_DIR}/lib/log4j-1.2.17.jar:${TAJS_DIR}/lib/nashorn-jb8u212-b1566.8.jar:${TAJS_DIR}/lib/test/hamcrest-core-1.3.jar:${TAJS_DIR}/lib/test/junit-4.12.jar:${TAJS_DIR}/lib/commons-text-1.9.jar:${TAJS_DIR}/lib/commons-lang3-3.12.0.jar:${TAJS_DIR}/antbuild:${TAJS_DIR}/test-resources:/usr/share/java/ant-launcher-1.10.9.jar:/usr/share/ant/lib/ant.jar:/usr/share/ant/lib/ant-junit.jar:/usr/share/ant/lib/ant-junit4.jar"
--- bash/scratch.sh
+++ bash/scratch.sh
@@ -6,47 +6,171 @@
 
 source "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/env.sh"
 
-RELATIVE_SOURCE_FILE="aws-bookstore-demo-app/functions/APIs/addToCart.js"
-BASE_OUT_DIR="${POLA_JS_DIR}/out/scratch"
-BASE_LOG_DIR="${POLA_JS_DIR}/out/log/scratch"
+cd "${POLA_JS_DIR}"
 
-SOURCE_FILE="${POLA_JS_DIR}/${RELATIVE_SOURCE_FILE}"
-OUT_DIR="${BASE_OUT_DIR}/${RELATIVE_SOURCE_FILE}"
-LOG_DIR="${BASE_LOG_DIR}/${RELATIVE_SOURCE_FILE}"
+trap 'jobs -p | xargs -r kill' EXIT
+
+SOURCE_DIR="aws-bookstore-demo-app/functions/APIs"
+SOURCE_FILE="aws-bookstore-demo-app/functions/APIs/addToCart.js"
+OUT_DIR="out/scratch"
 
 mkdir -p "${OUT_DIR}"
-mkdir -p "${LOG_DIR}"
+
+AE_MANIFESTS_DIR="${OUT_DIR}/ae-manifests"
+
+mkdir -p "${AE_MANIFESTS_DIR}"
 
 SWC_OUT_DIR="${OUT_DIR}/swc"
-RESULTS_DIR="${OUT_DIR}/results"
 
 mkdir -p "${SWC_OUT_DIR}"
-mkdir -p "${RESULTS_DIR}"
 
-time `# Time performing the following...` \
-  "${SWC}" `# Run swc` \
-  compile `# Compile source code` \
-  --out-dir "${SWC_OUT_DIR}" `# All swc output files go here` \
-  --source-maps true `# Generate source maps` \
-  --config jsc.target=es3 `# Compile to ECMAScript 3` \
-  "${SOURCE_FILE}" `# Compile this source file` \
-  |& tee "${LOG_DIR}/swc.log"
+SWC_RESULTS_OUT_DIR="${SWC_OUT_DIR}/results"
 
-time `# Time performing the following...` \
-  java `# Run java` \
-  -ea `# Enable assertions` \
-  dk.brics.tajs.FlowGraphOnlyMain `# Run "generate flowgraph only" main class` \
-  -out "${OUT_DIR}/tajs" `# Send TAJS output here` \
-  "${SWC_OUT_DIR}/${SOURCE_FILE}" `# Generate from this source file` \
-  |& tee "${LOG_DIR}/tajs.log"
+mkdir -p "${SWC_RESULTS_OUT_DIR}"
 
-time `# Time performing the following...` \
-  "${SOUFFLE}" `# Run souffle` \
-  -c `# Compile` \
-  -j "${NUM_CPUS}" `# Jobs` \
-  -F "${OUT_DIR}/tajs/flowgraphs/facts" `# Input facts` \
-  -I "${POLA_JS_DIR}/dl/include" `# Datalog source files include directory` \
-  "${POLA_JS_DIR}/dl/node-require-source-target.dl" `# Main datalog source file` \
-  -D "${OUT_DIR}/results" `# Output directory` \
-  `# -p "${OUT_DIR}/profile_c_j_72_no_frequency.prof" \ # Profiling` \
-  |& tee "${LOG_DIR}/souffle.log" # Souffle output log
+find "${SOURCE_DIR}" -type f | ag '\.(js|jsx|es6|es|mjs|ts|tsx)$' | sort | uniq > "${AE_MANIFESTS_DIR}/swc-inputs"
+
+cat "${AE_MANIFESTS_DIR}/swc-inputs" | sed 's/\.\(js\|jsx\|es6\|es\|mjs\|ts\|tsx\)$/.../g' | sort | uniq > "${AE_MANIFESTS_DIR}/swc-ellipsis"
+
+cat "${AE_MANIFESTS_DIR}/swc-ellipsis" | sed 's/\.\.\.$/.js/g' | sort | uniq > "${AE_MANIFESTS_DIR}/swc-js-outputs"
+
+cat "${AE_MANIFESTS_DIR}/swc-ellipsis" | sed 's/\.\.\.$/.js.map/g' | sort | uniq > "${AE_MANIFESTS_DIR}/swc-map-outputs"
+
+cat "${AE_MANIFESTS_DIR}/swc-js-outputs" "${AE_MANIFESTS_DIR}/swc-map-outputs" | sort | uniq > "${AE_MANIFESTS_DIR}/swc-outputs"
+
+echo "compile
+--out-dir
+${SWC_RESULTS_OUT_DIR}
+--source-maps
+true
+--config
+jsc.target=es3
+--ignore
+aws-bookstore-demo-app/functions/APIs/node_modules/**/test/shams/*.js
+${SOURCE_DIR}" > "${AE_MANIFESTS_DIR}/swc-args"
+
+echo "" > "${AE_MANIFESTS_DIR}/swc-env"
+
+"${ARTIFACT_EXECUTOR}" \
+  exec \
+    -e "${AE_MANIFESTS_DIR}/swc-env" \
+    -p "${SWC}" \
+    -a "${AE_MANIFESTS_DIR}/swc-args" \
+    -i "${AE_MANIFESTS_DIR}/swc-inputs" \
+    -o "${AE_MANIFESTS_DIR}/swc-outputs"
+
+# TIME_OUT_DIR="${OUT_DIR}/time"
+# SWC_OUT_DIR="${OUT_DIR}/swc"
+# TAJS_OUT_DIR="${OUT_DIR}/tajs"
+# SOUFFLE_OUT_DIR="${OUT_DIR}/souffle"
+
+# mkdir -p "${TIME_OUT_DIR}"
+# mkdir -p "${SWC_OUT_DIR}"
+# mkdir -p "${TAJS_OUT_DIR}"
+# mkdir -p "${SOUFFLE_OUT_DIR}"
+
+# SWC_RESULTS_OUT_DIR="${SWC_OUT_DIR}/results"
+
+# mkdir -p "${SWC_RESULTS_OUT_DIR}"
+
+# SEMAPHORIC_DIR=$(mktemp -d)
+# SEMAPHORIC_ID="scratch"
+# SEMAPHORIC_MAX_PROCS=$(expr ${NUM_CPUS} / 4)
+# SEMAPHORIC_POLL_SECS=1
+
+# function parallel () {
+#   "${SEMAPHORIC}" \
+#     --max-procs="${SEMAPHORIC_MAX_PROCS}" \
+#     --sem-dir="${SEMAPHORIC_DIR}" \
+#     --poll-secs="${SEMAPHORIC_POLL_SECS}" \
+#     "${SEMAPHORIC_ID}" \
+#     "$*" &
+# }
+
+# function mem_available_percent () {
+#   local -n map_output_value="$1"
+#   map_output_value=$(("100 * $(cat /proc/meminfo | grep 'MemAvailable' | grep -o '[0-9]\+') / $(cat /proc/meminfo | grep 'MemTotal' | grep -o '[0-9]\+')"))
+# }
+
+# while read -r JSON_FILE; do
+#   SRC="${JSON_FILE}"
+#   DST="$(echo "${JSON_FILE}" | sed -e 's/\/swc\//\/souffle\//g' -e 's/\.\//..\/2023-01-27_pola-js_scratch_data_collection\//g')"
+#   mkdir -p "$(dirname ${DST})"
+#   cp "${SRC}" "${DST}"
+# done < <(find ./out/scratch/swc/results -type f | ag '\.json$')
+
+# /usr/bin/env time `# Time performing the following...` \
+#   -o "${TIME_OUT_DIR}/swc.time" `# Put timing output here` \
+#     "${SWC}" `# Run swc` \
+#       compile `# Compile source code` \
+#       --out-dir "${SWC_RESULTS_OUT_DIR}" `# All swc output files go here` \
+#       --source-maps true `# Generate source maps` \
+#       --config jsc.target=es3 `# Compile to ECMAScript 3` \
+#       --ignore 'aws-bookstore-demo-app/functions/APIs/node_modules/**/test/shams/*.js' `# Ignore malformed test-only JavaScript` \
+#       "${SOURCE_DIR}" `# Compile this source file` \
+#       |& tee "${SWC_OUT_DIR}/swc.log" # Put SWC logging output here
+
+# while read -r JSON_FILE; do
+#   mkdir -p "${SWC_RESULTS_OUT_DIR}/$(dirname "${JSON_FILE}")"
+#   cp "${JSON_FILE}" "${SWC_RESULTS_OUT_DIR}/${JSON_FILE}"
+# done < <(find "${SOURCE_DIR}" | grep '\.json$')
+
+# while read -r SOURCE_FILE; do
+#   THIS_TIME_OUT_DIR="${TIME_OUT_DIR}/${SOURCE_FILE}"
+#   THIS_TAJS_OUT_DIR="${TAJS_OUT_DIR}/${SOURCE_FILE}"
+#   THIS_SOUFFLE_OUT_DIR="${SOUFFLE_OUT_DIR}/${SOURCE_FILE}"
+
+#   mkdir -p "${THIS_TIME_OUT_DIR}"
+#   mkdir -p "${THIS_TAJS_OUT_DIR}"
+#   mkdir -p "${THIS_SOUFFLE_OUT_DIR}"
+
+#   THIS_TAJS_RESULTS_DIR="${THIS_TAJS_OUT_DIR}/results"
+#   THIS_SOUFFLE_RESULTS_DIR="${THIS_SOUFFLE_OUT_DIR}/results"
+
+#   mkdir -p "${THIS_TAJS_RESULTS_DIR}"
+#   mkdir -p "${THIS_SOUFFLE_RESULTS_DIR}"
+
+#   parallel `# Run in parallel` \
+#     "(
+#       /usr/bin/env time `# Time performing the following...` \
+#         -o \"${THIS_TIME_OUT_DIR}/tajs.time\" `# Put timing output here` \
+#           java `# Run java` \
+#             -ea `# Enable assertions` \
+#             dk.brics.tajs.FlowGraphOnlyMain `# Run "generate flowgraph only" main class` \
+#             -out \"${THIS_TAJS_RESULTS_DIR}\" `# Send TAJS output here` \
+#             \"${SOURCE_FILE}\" `# Generate from this source file` \
+#             |& tee \"${THIS_TAJS_OUT_DIR}/tajs.log\" `# TAJS output log` && \
+#       /usr/bin/env time `# Time performing the following...` \
+#         -o \"${THIS_TIME_OUT_DIR}/souffle.time\" `# Put timing output here` \
+#           \"${SOUFFLE}\" `# Run souffle` \
+#           -c `# Compile` \
+#           -j \"${NUM_CPUS}\" `# Jobs` \
+#           -F \"${THIS_TAJS_RESULTS_DIR}/flowgraphs/facts\" `# Input facts` \
+#           -I \"${POLA_JS_DIR}/dl/include\" `# Datalog source files include directory` \
+#           \"${POLA_JS_DIR}/dl/node-require-source-target.dl\" `# Main datalog source file` \
+#           -D \"${THIS_SOUFFLE_RESULTS_DIR}\" `# Output directory` \
+#           `# -p \"${OUT_DIR}/profile_c_j_72_no_frequency.prof\" \ # Profiling` \
+#           |& tee \"${THIS_SOUFFLE_OUT_DIR}/souffle.log\" `# Souffle output log`
+#     )"
+# done < <(find "${SWC_RESULTS_OUT_DIR}" -type f | grep '\.js$')
+
+# time `# Time performing the following...` \
+#   java `# Run java` \
+#   -ea `# Enable assertions` \
+#   dk.brics.tajs.FlowGraphOnlyMain `# Run "generate flowgraph only" main class` \
+#   -out "${OUT_DIR}/tajs" `# Send TAJS output here` \
+#   "${SWC_OUT_DIR}/${SOURCE_FILE}" `# Generate from this source file` \
+#   |& tee "${LOG_DIR}/tajs.log"
+
+# time `# Time performing the following...` \
+#   "${SOUFFLE}" `# Run souffle` \
+#   -c `# Compile` \
+#   -j "${NUM_CPUS}" `# Jobs` \
+#   -F "${OUT_DIR}/tajs/flowgraphs/facts" `# Input facts` \
+#   -I "${POLA_JS_DIR}/dl/include" `# Datalog source files include directory` \
+#   "${POLA_JS_DIR}/dl/node-require-source-target.dl" `# Main datalog source file` \
+#   -D "${OUT_DIR}/results" `# Output directory` \
+#   `# -p "${OUT_DIR}/profile_c_j_72_no_frequency.prof" \ # Profiling` \
+#   |& tee "${LOG_DIR}/souffle.log" # Souffle output log
+
+wait
