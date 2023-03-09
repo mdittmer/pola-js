8237ad52335453efaa9714262426c8c301cc6139
Mark Dittmer <markdittmer@google.com>
2023-03-09 14:14:45 -0500

    Drop unnecessary code from analyze module; vistor / Visit implementation no longer has an implementation for every method

--- rust/testdata/sandbox.js
+++ rust/testdata/sandbox.js
@@ -1,15 +1,3 @@
-function x() {
-  let z = {y: () => console.log('Hello dynamic')};
-
-  with (z) {
-    y();
-  }
-
-  function y() {
-    console.log('Hello world');
-  }
-
-
+with (var x = {a: true}) {
+	console.log(a);
 }
-
-x();
